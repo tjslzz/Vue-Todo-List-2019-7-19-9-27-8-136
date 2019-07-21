cdd7397e3b9a79378ed5c3dd322ead1e5c66cd95
JarryLi <3244188372@qq.com>
2019-07-21 22:38:19 +0800

    [jerryLi] feat : add dbclick action and finish work !

--- src/components/to_do_item.vue
+++ src/components/to_do_item.vue
@@ -1,13 +1,27 @@
 <template>
 <li id="to_do_item" v-bind:class="{checked:item.key}">
     <input type="checkbox" v-model="item.key">
-    {{ item.value }}
+    <span @dblclick="editable" v-bind:contenteditable="edit" @input="updatable" ref="info">{{ item.value }}</span>
+    
 </li>
 </template>
 
 <script>
 export default {
     name: 'to_do_item',
-    props: {item:Object}
+    props: {item:Object},
+    data:()=>{
+        return{
+            edit:false
+        }
+    },
+    methods:{
+        editable(){
+            this.edit = true;
+        },
+        updatable(){
+            this.item.value = this.$refs.info.innerHTML;
+        }
+    }
 }
 </script>
